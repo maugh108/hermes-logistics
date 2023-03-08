e4982c2344a0e336151850d59339f07bec6c07f8
maugh108 <maugh108@outlook.com>
2023-03-07 20:44:56 -0600

    resolvers create user, order y trailer creados

--- src/resolvers.js
+++ src/resolvers.js
@@ -33,6 +33,41 @@ export const resolvers = {
             const user = new User(input)
             await user.save()
             return user
+        },
+        async createUser(_, { input }){
+            console.log(input)
+            let user = await User.findById(input.id)
+            if(!user) user = new User(input)
+            else{
+                user.firstName = input.firstName
+                user.lastName = input.lastName
+                user.username = input.username
+                user.password = input.password
+                user.age = input.age
+            }
+            await user.save()
+            return user
+        },
+        async createOrder(_, {input}){
+            let order = await Order.findById(input.id)
+            if(!order) order = new Order(input)
+            else{
+                order.number = input.number
+                order.pickup = input.pickup
+                order.status = input.status
+            }
+            await order.save()
+            return order
+        },
+        async createTrailer(_, {input}){
+            let trailer = await Trailer.findById(input.id)
+            if(!trailer) trailer = new Trailer(input)
+            else{
+                trailer.number = input.number
+                trailer.type = input.type
+            }
+            await trailer.save()
+            return trailer
         }
     }
 };
